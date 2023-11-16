8eb931b6299125a9c47506c7729ac32cc4b8fca6
Caleb <Calebtheeperson@gmail.com>
2023-11-16 13:31:33 -0800

    Added Bar, Shelf, Bottles, created vec of bottles

--- examples/engine-ecs-game/src/main.rs
+++ examples/engine-ecs-game/src/main.rs
@@ -1,20 +1,20 @@
 use engine::Spritesheet;
 use engine_ecs as engine;
+use engine_ecs::wgpu;
 use engine_ecs::{components::*, geom::*, Camera, SheetRegion};
 use hecs::Entity;
 use rand::Rng;
 type Engine = engine::Engine<Game>;
 
-// Components (markers)
+// Stuff for testing not really important
 struct Apple();
 struct Guy();
 
 // Bundles
-#[derive(hecs::Bundle)]
-struct WallBundle(Sprite, Transform, Solid, BoxCollision);
-#[derive(hecs::Bundle)]
+
 struct GuyBundle(Sprite, Transform, Pushable, BoxCollision, Physics, Guy);
 #[derive(hecs::Bundle)]
+
 struct AppleBundle(
     Sprite, //
     Transform,
@@ -23,26 +23,48 @@ struct AppleBundle(
     Physics,
     Apple,
 );
+/////
+
+//Bundles
+#[derive(hecs::Bundle)]
+struct EnvBundle(Sprite, Transform, Solid, BoxCollision);
+
 #[derive(hecs::Bundle)]
 struct DecoBundle(Sprite, Transform);
 
+//Bottle Bundle, feel free to add on as you see fit
+#[derive(hecs::Bundle)]
+struct BottleBundle(Sprite, Transform, Solid, BoxCollision);
+////
+
+///Width and height of the window
 const W: f32 = 320.0;
 const H: f32 = 240.0;
-const GUY_SPEED: f32 = 2.0;
-const GUY_SIZE: Vec2 = Vec2 { x: 16.0, y: 16.0 };
+/////
+
+//Testing Variables for Physics
 const APPLE_SIZE: Vec2 = Vec2 { x: 16.0, y: 16.0 };
+const SPRITE_MAX: usize = 16;
 const APPLE_MAX: usize = 128;
 const APPLE_INTERVAL: std::ops::Range<u32> = 1..10;
+const APPLE_SPEED_RANGE: std::ops::Range<f32> = (-2.0)..(-0.5);
+////
+
+//Static UVS
 const WALL_UVS: SheetRegion = SheetRegion::new(0, 0, 480, 12, 8, 8);
 const SHELF_UVS: SheetRegion = SheetRegion::new(0, 1, 50, 480, 264, 16);
-const APPLE_SPEED_RANGE: std::ops::Range<f32> = (-2.0)..(-0.5);
+const BAR_UVS: SheetRegion = SheetRegion::new(0, 36, 1, 480, 127, 47);
+const BOTTLE_UVS: SheetRegion = SheetRegion::new(0, 1, 1, 480, 3, 11);
+//////
+
+///Bundle Vectors
+const bottleBundles: Vec<&BottleBundle> = Vec::new();
+/// //
 
 struct Game {
     apple_timer: u32,
     score: u32,
-    // guy: Entity,
     spritesheet: engine::Spritesheet,
-    font: engine::BitFont,
 }
 
 impl engine::Game for Game {
@@ -61,80 +83,34 @@ impl engine::Game for Game {
                 .into_rgba8()
         };
         #[cfg(not(target_arch = "wasm32"))]
-        // let sprite_img = image::open("content/demo.png").unwrap().into_rgba8();
-        // let spritesheet = engine.add_spritesheet(&[&sprite_img], Some("demo spritesheet"));
-        // engine.spawn(DecoBundle(
-        //     Sprite(spritesheet, SheetRegion::new(0, 0, 0, 16, 640, 480)),
-        //     Transform {
-        //         x: W / 2.0,
-        //         y: H / 2.0,
-        //         w: W as u16,
-        //         h: H as u16,
-        //         rot: 0.0,
-        //     },
-        // ));
         let bar_img = image::open("content/bar_sheet.png").unwrap().into_rgba8();
         let spritesheet = engine.add_spritesheet(&[&bar_img], Some("The Bar"));
 
         //Spawning the bar in
-        engine.spawn(WallBundle(
-            Sprite(spritesheet, SheetRegion::new(0, 36, 1, 480, 127, 47)),
-            Transform {
-                x: W / 2.0,
-                y: 15.0,
-                w: W as u16,
-                h: 47 as u16,
-                rot: 0.0,
-            },
-            Solid::default(),
-            BoxCollision(AABB {
-                center: Vec2::ZERO,
-                size: Vec2 {
-                    x: W / 1.0,
-                    y: 47.0,
-                },
-            }),
-        ));
-
+        make_bar(spritesheet, engine, W / 2.0, 15.0, W, 47.0);
         make_shelf(spritesheet, engine, W / 2.0, 60.0 + 20.0, 160.0, 16.0);
         make_shelf(spritesheet, engine, W / 2.0, 100.0 + 20.0, 160.0, 16.0);
 
-        // Spawns a little dood\
-        engine.renderer.sprites.add
+        for i in 0..5 {
+            //Making bottles on bottom shelf
+            make_bottle(
+                spritesheet,
+                engine,
+                (W / 3.0) + (i * 20) as f32,
+                90.0,
+                3.0 * 2.6,
+                11.0 * 2.6,
+            );
+        }
 
-        // floor
-        // make_wall(spritesheet, engine, W / 2.0, 8.0, W, 16.0);
-        // // left wall
-        // make_wall(spritesheet, engine, 8.0, H / 2.0, 16.0, H);
-        // // right wall
-        // make_wall(spritesheet, engine, W - 8.0, H / 2.0, 16.0, H);
-        let font = engine.make_font(
-            spritesheet,
-            '0'..='9',
-            SheetRegion::new(0, 0, 512, 0, 80, 8),
-            10,
-        );
         Game {
             apple_timer: 0,
             score: 0,
-            font,
             spritesheet,
-            // guy,
         }
     }
     fn update(&mut self, engine: &mut Engine) {
-        if engine.frame_number() % 600 == 0 {
-            println!(
-                "{:.6} : {:.6} --- {:.6} : {:.6} --- {:.6} : {:.6} --- {:.6}",
-                engine.avg_sim_time(),
-                engine.max_sim_time(),
-                engine.avg_render_time(),
-                engine.max_render_time(),
-                engine.avg_net_time(),
-                engine.max_net_time(),
-                Self::DT
-            );
-        }
+        if engine.frame_number() % 600 == 0 {}
 
         //This handles user input and moved the guy accordingly
         // let dir = engine.input.key_axis(engine::Key::Left, engine::Key::Right);
@@ -150,46 +126,46 @@ impl engine::Game for Game {
         // };
 
         // This part of the code handeld spawning random apples
-        let mut rng = rand::thread_rng();
-        let mut apple_count = 0;
-        let mut to_remove = vec![];
-        for (apple, (_, trf)) in engine.world().query::<(&Apple, &Transform)>().iter() {
-            if trf.y < -8.0 {
-                to_remove.push(apple);
-            } else {
-                apple_count += 1;
-            }
-        }
-        for apple in to_remove {
-            engine.despawn(apple).unwrap();
-        }
-        if self.apple_timer > 0 {
-            self.apple_timer -= 1;
-        } else if apple_count < APPLE_MAX {
-            let _apple = engine.spawn(AppleBundle(
-                Sprite(self.spritesheet, SheetRegion::new(0, 0, 496, 4, 16, 16)),
-                Transform {
-                    x: rng.gen_range(8.0..(W - 8.0)),
-                    y: H + 8.0,
-                    w: APPLE_SIZE.x as u16,
-                    h: APPLE_SIZE.y as u16,
-                    rot: 0.0,
-                },
-                SolidPushable::default(),
-                BoxCollision(AABB {
-                    center: Vec2::ZERO,
-                    size: APPLE_SIZE,
-                }),
-                Physics {
-                    vel: Vec2 {
-                        x: 0.0,
-                        y: rng.gen_range(APPLE_SPEED_RANGE),
-                    },
-                },
-                Apple(),
-            ));
-            self.apple_timer = rng.gen_range(APPLE_INTERVAL);
-        }
+        // let mut rng = rand::thread_rng();
+        // let mut apple_count = 0;
+        // let mut to_remove = vec![];
+        // for (apple, (_, trf)) in engine.world().query::<(&Apple, &Transform)>().iter() {
+        //     if trf.y < -8.0 {
+        //         to_remove.push(apple);
+        //     } else {
+        //         apple_count += 1;
+        //     }
+        // }
+        // for apple in to_remove {
+        //     engine.despawn(apple).unwrap();
+        // }
+        // if self.apple_timer > 0 {
+        //     self.apple_timer -= 1;
+        // } else if apple_count < APPLE_MAX {
+        //     let _apple = engine.spawn(AppleBundle(
+        //         Sprite(self.spritesheet, SheetRegion::new(0, 1, 3, 4, 16, 16)),
+        //         Transform {
+        //             x: rng.gen_range(8.0..(W - 8.0)),
+        //             y: H + 8.0,
+        //             w: APPLE_SIZE.x as u16,
+        //             h: APPLE_SIZE.y as u16,
+        //             rot: 0.0,
+        //         },
+        //         SolidPushable::default(),
+        //         BoxCollision(AABB {
+        //             center: Vec2::ZERO,
+        //             size: APPLE_SIZE,
+        //         }),
+        //         Physics {
+        //             vel: Vec2 {
+        //                 x: 0.0,
+        //                 y: rng.gen_range(APPLE_SPEED_RANGE),
+        //             },
+        //         },
+        //         Apple(),
+        //     ));
+        //     self.apple_timer = rng.gen_range(APPLE_INTERVAL);
+        // }
     }
     fn handle_collisions(
         &mut self,
@@ -206,19 +182,7 @@ impl engine::Game for Game {
             }
         }
     }
-    fn render(&mut self, engine: &mut Engine) {
-        // This function draws the counter that was original used
-
-        // engine.draw_string(
-        //     &self.font,
-        //     self.score.to_string(),
-        //     Vec2 {
-        //         x: 16.0,
-        //         y: H - 16.0,
-        //     },
-        //     16.0,
-        // );
-    }
+    fn render(&mut self, engine: &mut Engine) {}
 }
 fn main() {
     Engine::new(winit::window::WindowBuilder::new()).run();
@@ -231,8 +195,8 @@ fn make_shelf(
     y: f32,
     w: f32,
     h: f32,
-) -> Entity {
-    engine.spawn(WallBundle(
+) {
+    let theBundle = EnvBundle(
         Sprite(spritesheet, SHELF_UVS),
         Transform {
             x,
@@ -246,5 +210,58 @@ fn make_shelf(
             center: Vec2::ZERO,
             size: Vec2 { x: 160.0, y: h },
         }),
+    );
+
+    engine.spawn(theBundle);
+}
+fn make_bar(
+    spritesheet: engine_ecs::Spritesheet,
+    engine: &mut Engine,
+    x: f32,
+    y: f32,
+    w: f32,
+    h: f32,
+) -> Entity {
+    engine.spawn(EnvBundle(
+        Sprite(spritesheet, BAR_UVS),
+        Transform {
+            x,
+            y,
+            w: w as u16,
+            h: h as u16,
+            rot: 0.0,
+        },
+        Solid::default(),
+        BoxCollision(AABB {
+            center: Vec2::ZERO,
+            size: Vec2 { x: W, y: 47.0 },
+        }),
     ))
+}
+
+fn make_bottle(
+    spritesheet: engine_ecs::Spritesheet,
+    engine: &mut Engine,
+    x: f32,
+    y: f32,
+    w: f32,
+    h: f32,
+) {
+    let theBundle = BottleBundle(
+        Sprite(spritesheet, BOTTLE_UVS),
+        Transform {
+            x,
+            y,
+            w: w as u16,
+            h: h as u16,
+            rot: 0.0,
+        },
+        Solid::default(),
+        BoxCollision(AABB {
+            center: Vec2::ZERO,
+            size: Vec2 { x: w, y: h },
+        }),
+    );
+    bottleBundles.push(&theBundle);
+    engine.spawn(theBundle);
 }
